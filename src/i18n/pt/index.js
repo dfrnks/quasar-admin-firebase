a84de577e17433d762d89ab20f97d43ed26ff77d
dfrnks <douglasfrancardoso@gmail.com>
2018-04-29 18:37:52 -0300

    started project

---
+++ src/i18n/pt/index.js
@@ -0,0 +1,12 @@
+// This is just an example,
+// so you can safely delete all default props below
+
+export default {
+  failed: 'Ação falhouu',
+  success: 'Ação executada com sucesso',
+  page: {
+    404: {
+      message: 'Sorry, nothing here...',
+    },
+  },
+};
---
+++ src/pages/users.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page class="flex flex-center">
+    <h3>Cadastro de usuarios</h3>
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: 'PageUsers',
+};
+</script>
